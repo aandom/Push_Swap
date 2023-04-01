f32b9b3daa7bf56a195b2ce5ac1af68fe2ce38f6
Alexander Andom <121858703+aandom@users.noreply.github.com>
2023-04-01 21:52:09 +0400

    updated

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -12,29 +12,41 @@
 
 #include "libft.h"
 
+int	set_sign(const char *str, int *sign, int i)
+{
+	int	c;
+
+	c = 0;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			*sign = *sign * -1;
+		c++;
+	}
+	return (c);
+}
+
 int	ft_atoi(const char *nptr)
 {
 	int		i;
 	long	num;
 	int		sign;
+	int		c;
 
 	i = 0;
 	num = 0;
 	sign = 1;
+	c = 0;
 	while ((nptr[i] >= 9 && nptr[i] <= 13) || nptr[i] == 32)
 		i++;
-	if (nptr[i] == '+' || nptr[i] == '-')
-	{
-		if (nptr[i] == '-')
-			sign = sign * -1;
-		i++;
-	}
+	i = i + set_sign(nptr, &sign, i);
 	while (ft_isdigit(nptr[i]))
 	{
 		num = (num * 10) + (nptr[i] - '0');
+		c++;
 		i++;
 	}
-	if (num * sign > INT_MAX || num * sign < INT_MIN)
+	if (num * sign > INT_MAX || num * sign < INT_MIN || c > 10)
 		return (0);
 	return ((int)num * sign);
 }
